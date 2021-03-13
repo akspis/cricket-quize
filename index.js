4363df56def19ab32d4d772a3b92e7d5195293fd
Akshay Pisal <akspis47@gmail.com>
2021-03-13 07:38:08 +0000

    updating style

--- index.js
+++ index.js
@@ -1,65 +1,71 @@
 var readlineSync = require("readline-sync");
+var chalk = require("chalk");
 
-var user = readlineSync.question("what is Your Name ");
+var score = 0;
+
+var user = readlineSync.question(chalk.bold.yellowBright("what is Your Name "));
 console.log("welcome " + user);
 
-var message = readlineSync.question("Are You Ready To Play Crikcket Quize ");
+var message = readlineSync.question(chalk.bold.yellowBright("Are You Ready To Play Crikcket Quize "));
 
 if(message === "yes"){
-console.log("Lets go " + user);
+console.log(chalk.underline.bold.bgBlue("Lets go " + user));
 }else{
-  console.log("its ok if you dont want to play quizev" + user)
+  console.log(chalk.underline.bold.bgBlue("its ok if you dont want to play quize " + user));
   process.exit(1)
 }
 function cricket(question, answer){
  
  var userAnswser = readlineSync.question(question);
 
- if(userAnswer.toUpperCase()===answer.toUpperCase()){
-   console.log("Wright");
+ if( userAnswser.toUpperCase()===answer.toUpperCase()){
+   console.log( chalk.underline.bold.bgMagenta("Wright"));
+   score++
  }else{
-   console.log("Wrong");
+   console.log(chalk.underline.bold.cyan("Wrong"));
  }
+ console.log(chalk.bold.yellowBright("Your current score "+ score));
+console.log("----------------");
 }
 
 var cricketList= [{
-question: "Who won the first World Cup, 1975?",
+question: chalk.bold.bgRed("Who won the first World Cup, 1975?"),
 answer:"west indies"       
 },
 {
-question: "How many times Mumbai Indians won the IPL?",
+question: chalk.bold.bgRed("How many times Mumbai Indians won the IPL?"),
 answer:"4"       
 },
 {
-question: "Which Australian city has a cricket ground called the Oval?",
+question: chalk.bold.bgRed("Which Australian city has a cricket ground called the Oval?"),
 answer:"Sydney"       
 },
 {
-question: "Who Captained India to victory in the World Cup in 1983?",
+question: chalk.bold.bgRed("Who Captained India to victory in the World Cup in 1983?"),
 answer:"Kapil Dev."       
 },
 {
-question: "How many teams are participating in the ICC World Cup 2019?",
+question: chalk.bold.bgRed("How many teams are participating in the ICC World Cup 2019?"),
 answer:"10"       
 },
 {
-question: "Which cricket team has won most ICC Cricket World Cup titles? ",
-answer:"Australia      
+question: chalk.bold.bgRed("Which cricket team has won most ICC Cricket World Cup titles? "),
+answer:"Australia"      
 },
 {
-question: "Which of the following country has hosted the ICC World Cup most times?",
+question: chalk.bold.bgRed("Which of the following country has hosted the ICC World Cup most times?"),
 answer:"england"       
 },
 {
-question: "How many minutes before play should the umpires take the field?",
+question: chalk.bold.bgRed("How many minutes before play should the umpires take the field?"),
 answer:"5"       
 },
 {
-question: "who is the indian captaine in 2011 world cup?",
+question: chalk.bold.bgRed("who is the indian captaine in 2011 world cup?"),
 answer:"Ms dhoni"       
 },
 {
-question: "The distance between the popping crease and the bowling crease is?",
+question: chalk.bold.bgRed("The distance between the popping crease and the bowling crease is?"),
 answer:"4 feet"       
 }
 ]
@@ -68,3 +74,8 @@ for(i = 0; i<cricketList.length; i++){
   const result = cricketList[i];
   cricket(result.question, result.answer);
 }
+console.log("_______________________");
+
+console.log(chalk.underline.bold.bgYellow("YAY !, Your Score Is:" + score));
+console.log("-----------------------");
+console.log(chalk.bold.bgGreenBright("Highest Score Holders:"));
